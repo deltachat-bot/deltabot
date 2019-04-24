1a6c108479dfe453431ab4ab14aba9f0d3159a76
Asiel Díaz Benítez <asieldbenitez@gmail.com>
2019-04-24 10:06:30 -0400

    Update __init__.py

--- src/simplebot/__init__.py
+++ src/simplebot/__init__.py
@@ -1,3 +1,20 @@
-#
+from abc import ABC, abstractmethod
+
 
 __version__ = "0.8.0"
+
+
+class Plugin(ABC):
+    """Interface for the bot's  plugins."""
+
+    name = None          # type: str
+    description = None   # type: str
+    version = None       # type: str
+    author = None        # type: str
+
+    def __init__(self, dc_account):
+      self.acc = dc_account
+
+    @abstractmethod
+    def process(self, msg):
+        """Return True if the message was processed with the plugin, False otherwise."""
