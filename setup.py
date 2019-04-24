045613b0fc78627640c4e49dc99a0c1f5d401ae5
Asiel Díaz Benítez <asieldbenitez@gmail.com>
2019-04-24 09:43:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import setuptools
 
 def main():
-    with open(os.path.join("src","deltabot", "__init__.py")) as f:
+    with open(os.path.join("src","simplebot", "__init__.py")) as f:
         for line in f:
             if "__version__" in line.strip():
                 version = line.split("=", 1)[1].strip().strip('"')
@@ -12,28 +12,27 @@ def main():
         long_desc = f.read()
 
     setuptools.setup(
-        name='deltabot',
-        description='Delta.Chat bot to reply to incoming messages in groups or 1:1 chats',
+        name='simplebot',
+        description='Delta Chat bot that does nothing, whew!',
         long_description = long_desc,
         version=version,
-        url='https://github.com/hpk42/deltabot',
+        url='https://github.com/adbenitez/simplebot',
         license='GPL',
         platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
-        author='holger krekel',
-        author_email='holger@merlinux.eu',
+        author='Asiel Díaz Benítez',
+        author_email='adbenitez@nauta.cu',
         package_dir={'': 'src'},
         packages = setuptools.find_packages('src'),
         classifiers=['Development Status :: 3 - Alpha',
                      'Intended Audience :: Developers',
-                     'License :: OSI Approved :: MIT License',
+                     'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
                      'Operating System :: POSIX',
                      'Operating System :: MacOS :: MacOS X',
                      'Topic :: Utilities',
-                     'Intended Audience :: Developers',
-                     'Programming Language :: Python'],
+                     'Programming Language :: Python :: 3'],
         entry_points='''
             [console_scripts]
-            deltabot=deltabot.cmdline:bot_main
+            simplebot=simplebot.cmdline:bot_main
         ''',
         install_requires = ["click>=6.0", "deltachat>=0.8.0"],
         zip_safe=False,
