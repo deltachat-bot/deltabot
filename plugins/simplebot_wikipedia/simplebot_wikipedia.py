2982e6d9b0d7f2d33dfc103fad0d205ce8a9888e
Asiel Díaz Benítez <asieldbenitez@gmail.com>
2019-05-08 10:44:22 -0400

    Update simplebot_wikipedia.py

--- plugins/simplebot_wikipedia/simplebot_wikipedia.py
+++ plugins/simplebot_wikipedia/simplebot_wikipedia.py
@@ -1,17 +1,24 @@
 from simplebot import Plugin
 
+import wikipedia
+
 
 class Wikipedia(Plugin):
 
     name = 'Wikipedia'
-    description = 'Provides the !wiki <text> command to search <text> in Wikipedia'
+    description = 'Provides the !w <text> command to search <text> in Wikipedia'
     version = '0.1.0'
-    author = 'sylar'
+    author = 'adbenitez'
+    #wikipedia.set_lang('es')
     
     def process(self, msg):
-        arg = self.get_args('!wiki', msg.text)
+        arg = self.get_args('!w', msg.text)
         if arg is not None:
+            try:
+                summary = wikipedia.summary(arg)
+            except wikipedia.PageError:
+                summary = 'Not found.'
             chat = self.acc.create_chat_by_message(msg)
-            chat.send_text('')
+            chat.send_text(arg+':\n\n'+summary)
             return True
         return False
