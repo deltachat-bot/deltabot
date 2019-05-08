1d9a87ddaf1f7bbca69090f14d5cbd7daca35ffe
Asiel Díaz Benítez <asieldbenitez@gmail.com>
2019-05-08 13:36:40 -0400

    Update setup.py

--- plugins/simplebot_wikipedia/setup.py
+++ plugins/simplebot_wikipedia/setup.py
@@ -8,26 +8,26 @@ CLASS_NAME = 'Wikipedia'
 with open(MODULE_NAME+'.py', 'rt', encoding='utf8') as fh:
     source = fh.read()
 PLUGIN_NAME = re.search(r'name = \'(.*?)\'', source, re.M).group(1)
-DESCRIPTION = re.search(r'description = \'(.*?)\'', source, re.M).group(1)
 VERSION = re.search(r'version = \'(.*?)\'', source, re.M).group(1)
 AUTHOR = re.search(r'author = \'(.*?)\'', source, re.M).group(1)
 AUTHOR_EMAIL = re.search(r'author_email = \'(.*?)\'', source, re.M).group(1)
-
+with open("README.rst", "r") as fh:
+LONG_DESCRIPTION = fh.read()
 
 setup(
     name=MODULE_NAME,
     version=VERSION,
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    description=DESCRIPTION,
-    long_description=DESCRIPTION,
+    description='A plugin for SimpleBot, a Delta Chat bot (http://delta.chat/)',
+    long_description=LONG_DESCRIPTION,
     long_description_content_type='text/x-rst',
     url='https://github.com/adbenitez/simplebot',
     classifiers=(
         'Development Status :: 3 - Alpha',
         'Environment :: Plugins',
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Topic :: Utilities'
     ),
