ae9d7fe7fe750d25b4ef0e45a3ffe6fd114b1329
adbenitez <asieldbenitez@gmail.com>
2019-05-23 07:49:57 -0400

    add info for platforms other than Android to getdelta plugin

--- plugins/simplebot_getdelta/simplebot_getdelta.py
+++ plugins/simplebot_getdelta/simplebot_getdelta.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 class GetDelta(Plugin):
 
     name = 'GetDelta'
-    description = 'Provides the !getdelta command to get info and links for the latest release of Delta Chat. Ex. !getdelta.'
+    description = 'Provides the !getdelta command to get info and links for the latest release of Delta Chat (iOS, Desktop or Android). Ex. !getdelta Android.'
     version = '0.1.0'
     author = 'adbenitez'
     author_email = 'adbenitez@nauta.cu'
@@ -16,18 +16,24 @@ class GetDelta(Plugin):
     def activate(cls, ctx):
         super().activate(ctx)
         if ctx.locale == 'es':
-            cls.description = 'Provee el comando !getdelta para obtener información y enlaces de la última versión de Delta Chat. Ej. !getdelta.'
+            cls.description = 'Provee el comando !getdelta para obtener información y enlaces de la última versión de Delta Chat(iOS, Desktop o Android). Ej. !getdelta Android.'
 
     @classmethod
     def process(cls, msg):
         arg = cls.get_args('!getdelta', msg.text)
         if arg is None:
             return False
-        page = urlopen('https://github.com/deltachat/deltachat-android/releases').read()
-        latest = BeautifulSoup(page, 'html.parser').find('div', class_='label-latest')
-        text = latest.ul.a['title']
-        text += latest.find('div', class_='markdown-body').get_text()
-        text += '\nhttps://github.com'+latest.find('div', class_='Box-body').a['href']
+        arg = arg.lower()
+        if arg == 'ios':
+            text = 'Delta Chat - iOS:\n\nhttps://testflight.apple.com/join/WVoYFOZe'
+        else:
+            platform = arg if arg == 'desktop' else 'android'
+            page = urlopen('https://github.com/deltachat/deltachat-{}/releases'.format(platform)).read()
+            latest = BeautifulSoup(page, 'html.parser').find('div', class_='label-latest')
+            text = 'Delta Chat - {}({}):\n\n'.format(platform.capitalize(), latest.ul.a['title'].trip())
+            text += latest.find('div', class_='markdown-body').get_text()
+            for a in list(latest.find('div', class_='Box-body').find_all('a'))[:-2]:
+                text += '\nhttps://github.com'+a['href']
         chat = cls.ctx.acc.create_chat_by_message(msg)
         chat.send_text(text)
         return True
