30c1fe66edea0249351ef6edc91ca7ca9bc5c235
adbenitez <asieldbenitez@gmail.com>
2019-05-24 14:51:17 -0400

    update ddg

--- plugins/simplebot_webgrabber/simplebot_webgrabber.py
+++ plugins/simplebot_webgrabber/simplebot_webgrabber.py
@@ -6,6 +6,23 @@ import bs4
 import requests
 
 
+def get_page(url, script=None):
+    headers = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
+    r = requests.get(url, headers=headers, stream=True)
+    if 'text/html' not in r.headers['content-type']:
+        r.connection.close()
+        return None
+    soup = bs4.BeautifulSoup(r.text, 'html.parser')
+    [t.extract() for t in soup(['script', 'meta', 'iframe', 'noscript', 'link'])]
+    comments = soup.find_all(text=lambda text:isinstance(text, bs4.Comment))
+    [comment.extract() for comment in comments]
+    if script is not None:
+        s = soup.new_tag('script')
+        s.string = script
+        soup.body.append(s)
+    return str(soup)
+
+
 class WebGrabber(Plugin):
 
     name = 'WebGrabber'
@@ -14,7 +31,7 @@ class WebGrabber(Plugin):
     author = 'adbenitez'
     author_email = 'adbenitez@nauta.cu'
     TEMP_FILE = 'page.html'
-    NOT_ALLOWED = 'Only html pages are allowed, but requested: {}'
+    NOT_ALLOWED = 'Only html pages are allowed'
     DOWNLOAD_FAILED = 'Falied to get the url: "{}"'
 
     @classmethod
@@ -22,7 +39,7 @@ class WebGrabber(Plugin):
         super().activate(ctx)
         if ctx.locale == 'es':
             cls.description = 'Provee el comando `!web <url>` el cual permite obtener la página web con la url dada. Ej. !web http://delta.chat.'
-            cls.NOT_ALLOWED = 'Solo está permitido descargar páginas web, pero el tipo solicitado es: {}'
+            cls.NOT_ALLOWED = 'Solo está permitido descargar páginas web'
             cls.DOWNLOAD_FAILED = 'No fue posible obtener la url: "{}"'
 
     @classmethod
@@ -35,28 +52,24 @@ class WebGrabber(Plugin):
             chat.send_text(cls.description)
         else:
             try:
-                headers = {'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'}
                 if not arg.startswith('http'):
                     arg = 'http://'+arg
-                r = requests.get(arg, headers=headers, stream=True)
-                if 'text/html' in r.headers['content-type']:
-                    soup = bs4.BeautifulSoup(r.text, 'html.parser')
-                    [t.extract() for t in soup(['script', 'meta', 'iframe', 'noscript', 'link'])]
-                    comments = soup.find_all(text=lambda text:isinstance(text, bs4.Comment))
-                    [comment.extract() for comment in comments]
-                    for a in soup.find_all('a', attrs={'href':True}):
-                        if a['href'].startswith('/'):
-                            index = r.url.find('/', 8)
-                            if index >= 0:                                
-                                a['href'] = r.url[:index]+a['href']
-                            else:
-                                a['href'] = r.url+a['href']
-                        a['href'] = 'mailto:{}?subject={}&body={}'.format(cls.ctx.acc.get_self_contact().addr, quote('!web '), quote(a['href'], safe=''))
+                script = r'for(let e of document.getElementsByTagName("a")){const h=e.href;h&&-1===h.indexOf("mailto:")&&(e.href="mailto:' + cls.ctx.acc.get_self_contact().addr + r'?subject=!web&body="+encodeURI(`${h}`))}'
+                page = get_page(arg, script)
+                if page is not None:
+                    # for a in soup.find_all('a', attrs={'href':True}):
+                    #     if a['href'].startswith('/'):
+                    #         index = r.url.find('/', 8)
+                    #         if index >= 0:                                
+                    #             a['href'] = r.url[:index]+a['href']
+                    #         else:
+                    #             a['href'] = r.url+a['href']
+                    #     a['href'] = 'mailto:{}?subject={}&body={}'.format(cls.ctx.acc.get_self_contact().addr, quote('!web '), quote(a['href'], safe=''))
                     with open(cls.TEMP_FILE, 'w') as fd:
-                        fd.write(str(soup))
+                        fd.write(page)
                     chat.send_file(cls.TEMP_FILE, mime_type='text/html')
                 else:
-                    chat.send_text(cls.NOT_ALLOWED.format(r.headers['content-type']))
+                    chat.send_text(cls.NOT_ALLOWED)
             except Exception as ex:
                 cls.ctx.logger.exception(ex)
                 chat.send_text(cls.DOWNLOAD_FAILED.format(arg))
