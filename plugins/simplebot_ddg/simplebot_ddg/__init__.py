525aca8c49f674a1a6a54b82b088cf79c5de73e8
adbenitez <asieldbenitez@gmail.com>
2019-05-24 16:25:40 -0400

    updated ddg

--- plugins/simplebot_ddg/simplebot_ddg/__init__.py
+++ plugins/simplebot_ddg/simplebot_ddg/__init__.py
@@ -55,8 +55,8 @@ class DuckDuckGo(Plugin):
         if arg is None:
             return False
         if arg:
-            script = r'for(let a of document.getElementsByTagName("a"))if(a.href&&-1===a.href.indexOf("mailto:")){const b=encodeURIComponent(`!web ${a.getAttribute("href").replace(/^(?!https?:\/\/|\/\/)\.?\/?(.*)/,`${"https://simonlaux.de"}/$1`)}`);a.href=`mailto:${"' + cls.ctx.acc.get_self_contact().addr + r'"}?subject=${b}&body=${b}`}'
-            text = get_page('https://duckduckgo.com/html?q={}'.format(quote_plus(arg)), script)
+            script = r'for(let a of document.getElementsByTagName("a"))if(a.href&&-1===a.href.indexOf("mailto:")){const b=encodeURIComponent(`${a.getAttribute("href").replace(/^(?!https?:\/\/|\/\/)\.?\/?(.*)/,`${"https://duckduckgo.com"}/$1`)}`);a.href=`mailto:${"' + cls.ctx.acc.get_self_contact().addr + r'"}?subject=%21web%20&body=${b}`}'
+            text = get_page('https://duckduckgo.com/lite?q={}'.format(quote_plus(arg)), script)
             #results = page.find_all('div', class_='result')
             # if not results:
             #     text = cls.NOT_FOUND.format(arg)
