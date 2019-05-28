08183ca314bad22f585ef33cdd77bc451c445739
adbenitez <asieldbenitez@gmail.com>
2019-05-28 12:00:07 -0400

    reworking plugins

--- plugins/simplebot_getdelta/simplebot_getdelta/__init__.py
+++ plugins/simplebot_getdelta/simplebot_getdelta/__init__.py
@@ -9,18 +9,27 @@ from jinja2 import Environment, PackageLoader, select_autoescape
 class GetDelta(Plugin):
 
     name = 'GetDelta'
-    description = 'Provides the !getdelta command.'
-    long_description = 'To get info and links for the latest release of Delta Chat (iOS, Desktop or Android). Ex. !getdelta Android'
+    description = 'Get info and links for the latest release of Delta Chat.'
+    long_description = 'You can get it sending the command !getdelta to this bot.'
     version = '0.2.0'
     author = 'adbenitez'
     author_email = 'adbenitez@nauta.cu'
     cmd = '!getdelta'
 
+    NOSCRIPT = 'You need a browser with JavaScript support for this page to work correctly.'
+
     @classmethod
     def activate(cls, ctx):
         super().activate(ctx)
-        # if ctx.locale == 'es':
-        #     cls.description = 'Provee el comando !getdelta para obtener información y enlaces de la última versión de Delta Chat(iOS, Desktop o Android). Ej. !getdelta Android.'
+        cls.TEMP_FILE = os.path.join(cls.ctx.basedir, cls.name+'.html')
+        cls.env = Environment(
+            loader=PackageLoader(__name__, 'templates'),
+            autoescape=select_autoescape(['html', 'xml'])
+        )
+        if ctx.locale == 'es':
+            cls.description = 'Obtén enlaces de descarga e información sobre la última versión de Delta Chat.'
+            cls.long_description = 'Puedes obtenerlo enviandole el comando !getdelta a este bot.'
+            cls.NOSCRIPT = 'Necesitas un navegador que soporte JavaScript para poder usar esta funcionalidad.'
 
     @classmethod
     def process(cls, msg):
@@ -28,16 +37,24 @@ class GetDelta(Plugin):
         if arg is None:
             return False
         arg = arg.lower()
-        if arg == 'ios':
-            text = 'Delta Chat ‒ iOS:\n\nhttps://testflight.apple.com/join/WVoYFOZe'
-        else:
-            platform = arg if arg == 'desktop' else 'android'
-            page = urlopen('https://github.com/deltachat/deltachat-{}/releases'.format(platform)).read()
-            latest = BeautifulSoup(page, 'html.parser').find('div', class_='label-latest')
-            text = 'Delta Chat ‒ {}({}):\n\n'.format(platform.capitalize(), latest.ul.a['title'].strip())
-            text += latest.find('div', class_='markdown-body').get_text()
-            for box in list(latest.find_all('div', class_='Box-body'))[:-2]:
-                text += '\nhttps://github.com{}\n'.format(box.a['href'])
+        ios = 'https://testflight.apple.com/join/WVoYFOZe'
+        android = cls.get_info('android')
+        desktop = cls.get_info('desktop')        
         chat = cls.ctx.acc.create_chat_by_message(msg)
-        chat.send_text(text)
+        template = cls.env.get_template('index.html')
+        html = template.render(plugin=cls, platforms=[('Android',android), ('iOS',ios), ('Desktop',desktop)])
+        with open(cls.TEMP_FILE, 'w') as fd:
+            fd.write(html)
+        chat = cls.ctx.acc.create_chat_by_message(msg)
+        chat.send_file(cls.TEMP_FILE, mime_type='text/html')
         return True
+
+    @staticmethod
+    def get_info(platform):
+        page = urlopen('https://github.com/deltachat/deltachat-{}/releases'.format(platform)).read()
+        latest = BeautifulSoup(page, 'html.parser').find('div', class_='label-latest')
+        text = '{}:<br>'.format(latest.ul.a['title'].strip())
+        text += latest.find('div', class_='markdown-body').get_text()
+        for box in list(latest.find_all('div', class_='Box-body'))[:-2]:
+            text += '<br>https://github.com{}'.format(box.a['href'])
+        return text
