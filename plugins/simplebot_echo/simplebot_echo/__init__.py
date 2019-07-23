a4e11c849878340828f0a677c07cfb359a65917b
adbenitez <asieldbenitez@gmail.com>
2019-07-23 01:32:25 -0400

    use gettext

--- plugins/simplebot_echo/simplebot_echo/__init__.py
+++ plugins/simplebot_echo/simplebot_echo/__init__.py
@@ -7,8 +7,6 @@ from simplebot import Plugin
 class Echo(Plugin):
 
     name = 'Echo'
-    description = 'Simple plugin to reply back a message.'
-    long_description = 'To use it you can simply send a message starting with the command !echo. For example:\n!echo hello world'
     version = '0.2.0'
     author = 'adbenitez'
     author_email = 'adbenitez@nauta.cu'
@@ -17,10 +15,14 @@ class Echo(Plugin):
     @classmethod
     def activate(cls, ctx):
         super().activate(ctx)
-        # if ctx.locale == 'es':
-        #     cls.description = 'Un plugin sencillo para que el bot repita lo que envíes como un eco.'
-        #     cls.long_description = 'Para usarlo puedes enviar un mensaje que comience con !echo, por ejemplo: !echo hola mundo y el bot responderá "hola mundo", esto permite comprobar de forma rápida que el bot está funcionando.'
-
+        localedir = os.path.join(os.path.dirname(__file__), 'locale')
+        lang = gettext.translation('simplebot_echo', localedir=localedir,
+                                   languages=[ctx.locale], fallback=True)
+        lang.install()
+        
+        cls.description = _('Simple plugin to reply back a message.')
+        cls.long_description = _('To use it you can simply send a message starting with the command !echo. For example:\n!echo hello world')
+    
     @classmethod
     def process(cls, msg):
         text = cls.get_args(cls.cmd, msg.text)
