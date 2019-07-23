67f5cdf24733e31996d86d598f8e218e18c85430
adbenitez <asieldbenitez@gmail.com>
2019-07-23 16:10:10 -0400

    simplebot_echo: don't use English strings as id

--- plugins/simplebot_echo/simplebot_echo/__init__.py
+++ plugins/simplebot_echo/simplebot_echo/__init__.py
@@ -17,12 +17,15 @@ class Echo(Plugin):
     def activate(cls, ctx):
         super().activate(ctx)
         localedir = os.path.join(os.path.dirname(__file__), 'locale')
-        lang = gettext.translation('simplebot_echo', localedir=localedir,
-                                   languages=[ctx.locale], fallback=True)
+        try:
+            lang = gettext.translation('simplebot_echo', localedir=localedir,
+                                       languages=[ctx.locale])
+        except OSError:
+            lang = gettext.translation('simplebot_echo', localedir=localedir,
+                                       languages=['en'])
         lang.install()
-        
-        cls.description = _('Simple plugin to echo back a message.')
-        cls.long_description = _('To use it you can simply send a message starting with the command !echo. For example:\n!echo hello world')
+        cls.description = _('plugin-description')
+        cls.long_description = _('plugin-long-description')
     
     @classmethod
     def process(cls, msg):
