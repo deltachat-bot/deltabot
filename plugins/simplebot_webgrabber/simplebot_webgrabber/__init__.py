69804daec092b664f0daed57c20795a1d176e227
adbenitez <asieldbenitez@gmail.com>
2019-08-21 01:14:38 -0400

    fix typo

--- plugins/simplebot_webgrabber/simplebot_webgrabber/__init__.py
+++ plugins/simplebot_webgrabber/simplebot_webgrabber/__init__.py
@@ -47,7 +47,7 @@ class WebGrabber(Plugin):
             ('/wt', ['<text>'], _('Search in Wiktionary'), cls.wt_cmd),
             ('/w', ['<text>'], _('Search in Wikipedia'), cls.w_cmd),
             ('/wttr', ['<text>'],
-             _('Search weather info from wttr.in'), cls.w_cmd),
+             _('Search weather info from wttr.in'), cls.wttr_cmd),
             ('/web', ['<url>'], _('Get a webpage or file'), cls.web_cmd),
             ('/web/app', [], _('Sends an html app to help you to use the plugin.'), cls.app_cmd)]
         cls.bot.add_commands(cls.commands)
