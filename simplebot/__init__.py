0efc26de87d57890c5eaaa3d2ad7900642323e3e
adbenitez <asieldbenitez@gmail.com>
2019-11-12 18:28:09 -0500

    use .htmlzip instead of .html.zip

--- simplebot/__init__.py
+++ simplebot/__init__.py
@@ -128,7 +128,7 @@ class SimpleBot(DeltaBot):
 
     def send_html(self, chat, html, basename, mode):
         if mode in (Mode.HTMLZIP, Mode.TEXT_HTMLZIP):
-            file_path = self.get_blobpath(basename+'.html.zip')
+            file_path = self.get_blobpath(basename+'.htmlzip')
             zlib.Z_DEFAULT_COMPRESSION = 9
             with zipfile.ZipFile(file_path, 'w', compression=zipfile.ZIP_DEFLATED) as fd:
                 fd.writestr('index.html', html)
