60c913a9e383d62fcc9afaccb1b4dc06e87073d1
adbenitez <asieldbenitez@gmail.com>
2019-11-16 21:35:23 -0500

    don't wrap urls in rss plugin

--- plugins/simplebot_rss/simplebot_rss/__init__.py
+++ plugins/simplebot_rss/simplebot_rss/__init__.py
@@ -16,6 +16,7 @@ import requests
 
 
 feedparser.USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'
+html2text.config.WRAP_LINKS = False
 
 
 class RSS(Plugin):
