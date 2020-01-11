b58201973891c1e80e229990a53b5fff3391364b
adbenitez <asieldbenitez@gmail.com>
2020-01-11 12:30:50 -0500

    add /tr/list command, to list supported languages

--- plugins/simplebot_translator/simplebot_translator/__init__.py
+++ plugins/simplebot_translator/simplebot_translator/__init__.py
@@ -3,13 +3,18 @@ import gettext
 import os
 
 from simplebot import Plugin, PluginCommand
+from translators.google import LANGUAGES
 import translators as ts
+
+
+LANGUAGES = '\n'.join(['* {}: {}'.format(v, k)
+                       for k, v in sorted(LANGUAGES.items(), key=lambda e: e[1])])
 
 
 class Translator(Plugin):
 
     name = 'Translator'
-    version = '0.3.0'
+    version = '0.4.0'
 
     @classmethod
     def activate(cls, bot):
@@ -20,7 +25,9 @@ class Translator(Plugin):
         lang.install()
         cls.description = _('Translate text. Example: /tr en es hello world')
         cls.commands = [
-            PluginCommand('/tr', ['<lang1>', '<lang2>', '<text>'], _('Translate text from lang1 to lang2'), cls.tr_cmd)]
+            PluginCommand('/tr', ['<lang1>', '<lang2>', '<text>'],
+                          _('Translate text from lang1 to lang2'), cls.tr_cmd),
+            PluginCommand('/tr/list', [], _('List supported languages and their code'), cls.tr_list)]
         cls.bot.add_commands(cls.commands)
 
     @classmethod
@@ -32,3 +39,7 @@ class Translator(Plugin):
             l1, l2, text = ctx.text.split(maxsplit=2)
             chat.send_text(ts.google(text=text, from_language=l1,
                                      to_language=l2, host='https://translate.google.com'))
+
+    @classmethod
+    def tr_list(cls, ctx):
+        cls.bot.get_chat(ctx.msg).send_text(LANGUAGES)
