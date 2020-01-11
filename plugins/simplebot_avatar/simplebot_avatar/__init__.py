1955dd3612675070324a7d8718e61060af9ba362
adbenitez <asieldbenitez@gmail.com>
2020-01-11 17:27:28 -0500

    add /avatar/bird command

--- plugins/simplebot_avatar/simplebot_avatar/__init__.py
+++ plugins/simplebot_avatar/simplebot_avatar/__init__.py
@@ -30,15 +30,27 @@ class Avatar(Plugin):
         cls.long_description = _(
             'Uses: https://www.peppercarrot.com/extras/html/2016_cat-generator')
         cls.commands = [
-            PluginCommand('/avatar', ['[text]'], _('Generates an avatar based on the given text, if no text is given a random avatar is generated'), cls.avatar_cmd)]
+            PluginCommand('/avatar', ['[text]'], _(
+                'Generates a cat avatar based on the given text, if no text is given a random avatar is generated'), cls.avatar_cmd),
+            PluginCommand('/avatar/bird', ['[text]'], _('Generates a bird avatar based on the given text, if no text is given a random avatar is generated'), cls.bird_cmd)]
         cls.bot.add_commands(cls.commands)
 
     @classmethod
     def avatar_cmd(cls, ctx):
+        cls._avatar_cmd(
+            ctx, 'https://www.peppercarrot.com/extras/html/2016_cat-generator/')
+
+    @classmethod
+    def bird_cmd(cls, ctx):
+        cls._avatar_cmd(
+            ctx, 'https://www.peppercarrot.com/extras/html/2019_bird-generator/')
+
+    @classmethod
+    def _avatar_cmd(cls, ctx, url):
         chat = cls.bot.get_chat(ctx.msg)
-        url = 'https://www.peppercarrot.com/extras/html/2016_cat-generator/'
+
         if not ctx.text:
-            with requests.get(url+'index.php', headers=HEADERS) as r:
+            with requests.get(url, headers=HEADERS) as r:
                 r.raise_for_status()
                 soup = bs4.BeautifulSoup(r.text, 'html.parser')
             ctx.text = soup.find('img', class_='avatar')[
