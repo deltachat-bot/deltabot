ddbe25af9ff5d09608d07aab1c7736d3de558cff
holger krekel <holger@merlinux.eu>
2020-04-03 11:47:36 +0200

    refactored logging to become a plugin

--- src/deltabot/plugins.py
+++ src/deltabot/plugins.py
@@ -42,7 +42,7 @@ def get_global_plugin_manager():
 
 
 def make_plugin_manager():
-    from deltabot.builtin import echo, db, cmdline
+    from deltabot.builtin import echo, db, cmdline, log
 
     pm = pluggy.PluginManager(spec_name)
     pm.add_hookspecs(DeltaBotSpecs)
@@ -51,5 +51,6 @@ def make_plugin_manager():
     pm.register(plugin=echo, name=".builtin.echo")
     pm.register(plugin=db, name=".builtin.db")
     pm.register(plugin=cmdline, name=".builtin.cmdline")
+    pm.register(plugin=log, name=".builtin.log")
     pm.check_pending()
     return pm
