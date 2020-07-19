981b231757d4b1e69f57fb14dca600e5d7121951
holger krekel <holger@merlinux.eu>
2020-07-19 14:08:05 +0200

    show that you can add a filename

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -25,3 +25,19 @@ def test_simple_filter(bot_tester):
     assert msg_reply.text == "correct answer!"
     msg_reply = bot_tester.send_command("hello 10")
     assert msg_reply.text == "try again!"
+
+
+def test_pseudo_downloader(bot_tester):
+    def downloader(message, replies):
+        """ pseudo downloader of https"""
+        if "https" in message.text:
+            fn = __file__
+            replies.add(text="downloaded", filename=__file__)
+
+    bot_tester.bot.filters.register(name="downloader", func=downloader)
+    msg_reply = bot_tester.send_command("this https://qwjkeqwe")
+    assert msg_reply.text == "downloaded"
+    assert msg_reply.filename != __file__  # blobdir
+    with open(msg_reply.filename) as f:
+        content = f.read()
+    assert content == open(__file__).read()
